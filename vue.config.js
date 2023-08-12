9fbbca6ef6f8620f8e0df644e486657598a15901
Eliseu dos Santos da Silva <s.eliseu.santos@gmail.com>
2023-08-12 02:07:02 -0300

    First commit creating structure

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: { 
+    output: { libraryTarget: "system", }, 
+  }, 
+  chainWebpack: (config) => { 
+    if (config.plugins.has("SystemJSPublicPathWebpackPlugin")) { 
+      config.plugins.delete("SystemJSPublicPathWebpackPlugin"); 
+    }
+  }, 
+  filenameHashing: false,
+})
